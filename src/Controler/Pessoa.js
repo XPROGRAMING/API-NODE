ea0edafd2b76d8ca4c46b613ff0f8f14ff2755ce
Michael Henrique <michaelhenrique@Air-de-Michael.local>
2024-04-01 13:01:45 -0300

    segunda

--- src/Controler/Pessoa.js
+++ src/Controler/Pessoa.js
@@ -1,6 +1,17 @@
 import { openDB } from "../configDB.js"; //aqui importo a funcao assincrona chamada createTable
 
 export async function createTable(){
+
+    /*
+    if (!pessoa || !pessoa.nome || !pessoa.idade) {
+        throw new Error('Os campos obrigatórios (nome e idade) não foram fornecidos.');
+        !pessoa -> verifica se o objeto está vazio, ! siginifca negacao
+        || operador logico "ou"
+        throw serve ara lancar o erro de forma manualmente
+
+    }
+    */
+
     openDB().then(db => { //a funcao openDB é chamada, quando a promessa é resolvida, o codigo 
         //dentro da funcao de retorno de then é executado then serve para tratar o resultado que deu certo
         //o catch() para tratar qualquer erro que ocorra durnte a execucao da promessa
@@ -10,8 +21,23 @@ export async function createTable(){
 }
 
 export async function insertPessoa(pessoa){
-    openDB().then(db => { 
+    openDB()
+    .then(db => { 
         db.run('INSERT INTO Pessoa (nome,idade) VALUES (?,?)', [pessoa.nome, pessoa.idade])
         // Values(?,?) -> serve para monstrar onde os valores fornecidos devem ser inseridos 
     })
+    //para tratar excessao ao criar banco de dados
+    /* .catch(error => {
+        console.error('Erro ao abrir o banco de dados:', error);
+        
+    }); */
+}
+
+export async function updatePessoa(pessoa){
+    openDB()
+    .then(db => { 
+        db.run('UPDATE Pessoa SET nome=? ,idade=? WHERE id=?', [pessoa.nome, pessoa.idade, pessoa.id])
+        
+    })
+   
 }
--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 //import { openDB } from './configDB.js'
-import { createTable, insertPessoa } from './Controler/Pessoa.js'
+import { createTable, insertPessoa, updatePessoa } from './Controler/Pessoa.js'
 import express from 'express'
 const app = express()
 app.use(express.json())
 
 createTable()
-
+//rota raiz
 app.get('/', (req, res) =>{
     res.send("teste")
 })
 
+//rota insert
 app.post('/pessoa', (req,res) =>{
     insertPessoa(req.body)
     res.json({
         "statuscCode": 200
     })
+})
+//rota update 
+app.put('/pessoa', (req,res) =>{
+    if(req.body && !req.body.id){
+        res.json({
+            "statusCode":"400",
+            "msg":"Voce precisa informar um id"
+        })
+    } else {
+        updatePessoa(req.body)
+        res.json({
+            "statuscCode": 200
+        })
+    }
 })
 
 
